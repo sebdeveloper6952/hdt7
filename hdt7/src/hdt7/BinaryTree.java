8e103f1af147a1978f26ad1bf65d3cbad11ea2b5
sevic69 <sebdeveloper6952@gmail.com>
2018-03-09 11:34:11 -0600

    Initial commit.

---
+++ hdt7/src/hdt7/BinaryTree.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt7;
+
+/**
+ * Sebastian Arriola 11463
+ * Fernando Figueroa 14175
+ * Jose Andres Arenas 14470
+ */
+public class BinaryTree<E>
+{
+    
+}
---
+++ hdt7/src/hdt7/IBinaryTree.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt7;
+
+/**
+ *
+ * @author sevic69
+ */
+public interface IBinaryTree<E>
+{
+    
+}
